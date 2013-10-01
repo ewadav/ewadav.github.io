987086c9832db7fba10c3bf5a64cb19ec59cfe6a
David Ewald <ewadav@uw.edu>
2013-10-01 12:13:56 -0700

    Layout is good, name is wrapped though, also getting flashing on the scroll

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
 
+$(window).on('scroll', function () {
+    var sidebar = $('.left-nav');
+    var scrollTop     = $(window).scrollTop(),
+        elementOffset = sidebar.offset().top,
+        distance      = (elementOffset - scrollTop);
+    if(distance < 10) {
+    	sidebar.addClass('sticky');
+    }else{
+    	sidebar.removeClass('sticky');
+    }
+});
