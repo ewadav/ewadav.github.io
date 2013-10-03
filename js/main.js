f5272cc5c97941afc36da1c1f28072304d28077c
David Ewald <ewadav@uw.edu>
2013-10-02 18:17:35 -0700

    Trying some things out with transitions of the sections. Need to have default white space on the right and such

--- js/main.js
+++ js/main.js
@@ -1,12 +1,10 @@
 
-/* $(window).on('scroll', function () {
-    var sidebar = $('.left-nav');
+$(window).on('scroll', function () {
+    var sidebar = $('.main-title');
     var scrollTop     = $(window).scrollTop(),
         elementOffset = sidebar.offset().top,
         distance      = (elementOffset - scrollTop);
-    if(distance < 10 && !sidebar.hasClass('sticky')) {
-    	sidebar.addClass('sticky');
-    }else if (distance > 10) {
-    	sidebar.removeClass('sticky');
+    if(distance < 300 && !$('#overview').is(":visible")) {
+    	$('#overview').show("slide");
     }
-}); */
+}); 
